b1bee6944d02a49e8c19b86a9cc0950d8b4f7ff0
MDJAmin <mohammadamindf@gmail.com>
2025-02-02 20:29:08 +0330

    Fix: Route 0.9

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-
-// const basename = "/Game_Zone";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
